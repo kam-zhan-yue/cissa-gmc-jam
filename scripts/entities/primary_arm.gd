f22a589020e95f4424f0f952c2f5c6d52d5e8d83
Alex Kam <kamzhanyue@gmail.com>
2025-03-08 10:31:21 +1100

    feat: implement arm movement

--- scripts/entities/primary_arm.gd
+++ scripts/entities/primary_arm.gd
@@ -2,14 +2,14 @@ class_name PrimaryArm
 extends Arm
 
 const MAX_REACH = 100.0
-const ARM_SPEED = 100.0
+const ARM_SPEED = 300.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	var aim_vertical := Input.get_axis("player_1_aim_down", "player_1_aim_up")
 	var aim_horizontal := Input.get_axis("player_1_aim_left", "player_1_aim_right")
 	var target_velocity := Vector2(aim_horizontal, -aim_vertical)
 	self.aim += target_velocity * delta * ARM_SPEED
-
-	print(self.aim)
+	if self.aim.length() >= MAX_REACH:
+		self.aim = self.aim.normalized() * MAX_REACH
 	queue_redraw()
