d28d4285404f0977dfc60755043839d0f404451f
kamzhanyue <kamzhanyue@gmail.com>
2025-03-08 23:43:56 +1100

    refactor: tidy up code

--- scripts/entities/arm.gd
+++ scripts/entities/arm.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var aim := Vector2.ZERO
 
+@export var use_global_position := false
 @onready var hand: Area2D = %Hand
 
 var constraints: Array[Constraint] = []
@@ -12,40 +13,24 @@ func _ready() -> void:
 		constraints.append(Constraint.new(Vector2.ZERO, 10.0, 0.0))
 
 func _get_hand_position() -> Vector2:
-	return position + self.aim
+	return _get_body_position() + self.aim
+
+func _get_body_position() -> Vector2:
+	return global_position if use_global_position else position
 
 func _draw():
 	_draw_fabrik()
-
-func _draw_constraint(i: int) -> void:
-	var node_distance = PrimaryArm.MAX_REACH / len(constraints)
-	var godot_blue : Color = Color("478cbf")
-	if i == 0:
-		constraints[i].position = _get_hand_position()
-		draw_circle(_get_hand_position(), constraints[i].radius, godot_blue)
-	else:
-		var prev = constraints[i-1]
-		var curr = constraints[i]
-		var difference = curr.position - prev.position
-		# If the constraint is past the distance per node, we wanna lock it
-		if difference.length() >= node_distance:
-			var new_position = prev.position + difference.normalized() * node_distance
-			constraints[i].position = new_position
-			draw_circle(new_position, constraints[i].radius, godot_blue)
-		else:
-			draw_circle(curr.position, constraints[i].radius, godot_blue)
 			
 func _draw_fabrik() -> void:
-	_draw_backwards()
-	_draw_forwards()
+	_process_backwards()
+	_process_forwards()
+	_draw_constraints()
 
-func _draw_backwards() -> void:
-	var godot_blue : Color = Color("478cbf")
+func _process_backwards() -> void:
 	var node_distance := PrimaryArm.MAX_REACH / len(constraints)
 	# Set the end constraint to the end effector
 	var target_pos = _get_hand_position()
 	constraints[-1].position = target_pos
-	draw_circle(constraints[-1].position, constraints[-1].radius, godot_blue)
 	# We want to update all the constraint positions up to the first constraint
 	# We don't want to set the angle and direction of the first constraint
 	for i in range(len(constraints) - 1, 0, -1):
@@ -58,20 +43,24 @@ func _draw_backwards() -> void:
 			var new_position := curr_node + difference.normalized() * node_distance
 			constraints[i - 1].position = new_position
 
-func _draw_forwards() -> void:
-	var godot_blue : Color = Color("478cbf")
+func _process_forwards() -> void:
 	var node_distance := PrimaryArm.MAX_REACH / len(constraints)
 	# Set the start constraint to the origin
-	constraints[0].position = position
-	draw_circle(constraints[0].position, constraints[0].radius, godot_blue)
+	constraints[0].position = _get_body_position()
 	for i in range(len(constraints) - 1):
 		var next_node := constraints[i + 1].position
 		var curr_node := constraints[i].position
 		var difference := next_node - curr_node
 		
+		# If the constraint is past the distance per node, we wanna lock it
 		if difference.length() >= node_distance:
 			var new_position := curr_node + difference.normalized() * node_distance
 			constraints[i + 1].position = new_position
-			draw_circle(new_position, constraints[i + 1].radius, godot_blue)
-		else:
-			draw_circle(next_node, constraints[i + 1].radius, godot_blue)
+
+func _draw_constraints() -> void:
+	for i in range(len(constraints)):
+		_draw_constraint(constraints[i])
+
+func _draw_constraint(constraint: Constraint) -> void:
+	var godot_blue : Color = Color("478cbf")
+	draw_circle(constraint.position, constraint.radius, godot_blue)
