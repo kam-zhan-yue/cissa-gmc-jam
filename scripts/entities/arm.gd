ade8fdb49ca1f70e4d15fa5d8ed29f050ed0024a
Alex Kam <kamzhanyue@gmail.com>
2025-03-08 22:34:42 +1100

    chore: start on constraints class

--- scripts/entities/arm.gd
+++ scripts/entities/arm.gd
@@ -5,9 +5,34 @@ var aim := Vector2.ZERO
 
 @onready var hand: Area2D = %Hand
 
+var constraints: Array[Constraint] = []
+
+func _ready() -> void:
+	for i in 10:
+		constraints.append(Constraint.new(Vector2.ZERO, 10.0))
+
 func _get_hand_position() -> Vector2:
 	return position + self.aim
 
 func _draw():
+	for i in range(len(constraints)):
+		_draw_constraint(i)
+		
+
+func _draw_constraint(i: int) -> void:
+	var node_distance = PrimaryArm.MAX_REACH / len(constraints)
 	var godot_blue : Color = Color("478cbf")
-	draw_circle(_get_hand_position(), 10, godot_blue)
+	if i == 0:
+		constraints[i].position = _get_hand_position()
+		draw_circle(_get_hand_position(), constraints[i].radius, godot_blue)
+	else:
+		var prev = constraints[i-1]
+		var curr = constraints[i]
+		var distance = prev.position - curr.position
+		# If the constraint is past the distance per node, we wanna lock it
+		if distance.length() >= node_distance:
+			var new_position = prev.position - distance
+			constraints[i].position = new_position
+			draw_circle(new_position, constraints[i].radius, godot_blue)
+			
+		
---
+++ scripts/entities/constraint.gd
@@ -0,0 +1,10 @@
+class_name Constraint
+extends Object
+
+var position: Vector2
+var radius: float
+
+
+func _init(p: Vector2 = Vector2.ZERO, r: float = 0.0) -> void:
+	self.position = p
+	self.radius = r
