a3ff85672b48b590722b261ac9627289ec0383f3
kamzhanyue <kamzhanyue@gmail.com>
2025-03-08 22:49:04 +1100

    feat:  implement basic worm animation

--- scripts/entities/arm.gd
+++ scripts/entities/arm.gd
@@ -28,11 +28,14 @@ func _draw_constraint(i: int) -> void:
 	else:
 		var prev = constraints[i-1]
 		var curr = constraints[i]
-		var distance = prev.position - curr.position
+		var difference = curr.position - prev.position
 		# If the constraint is past the distance per node, we wanna lock it
-		if distance.length() >= node_distance:
-			var new_position = prev.position - distance
+		if difference.length() >= node_distance:
+			var new_position = prev.position + difference.normalized() * node_distance
 			constraints[i].position = new_position
 			draw_circle(new_position, constraints[i].radius, godot_blue)
+		else:
+			draw_circle(curr.position, constraints[i].radius, godot_blue)
+			
 			
 		
