c6b1ad6cc509d250ee7d954925a6ec4646f3cfd2
kamzhanyue <kamzhanyue@gmail.com>
2025-03-09 23:51:56 +1100

    feat: implement ink popup

--- scripts/ui/ink_popup.gd
+++ scripts/ui/ink_popup.gd
@@ -1,7 +1,11 @@
 class_name InkPopup
 extends Control
 
+const GAME_SETTINGS = preload("res://resources/game_settings.tres")
+@onready var ink_container: MarginContainer = %InkContainer
+
 var player_id := 0
+var max_ink := 0.0
 
 func init(id: int) -> void:
 	self.player_id = id
@@ -9,4 +13,4 @@ func init(id: int) -> void:
 	_on_ink_changed(Game.get_player(id).ink)
 
 func _on_ink_changed(ink: float) -> void:
-	pass
+	ink_container.scale.x = ink / GAME_SETTINGS.max_ink
